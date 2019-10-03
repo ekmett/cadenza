2dc2221978d289cc3d40cd897feae262951e66be
Edward Kmett <ekmett@gmail.com>
2019-10-03 07:15:39 -0700

    clean up detector

--- src/main/java/core/Detector.java
+++ src/main/java/core/Detector.java
@@ -1,36 +1,18 @@
 package core;
 
 import com.oracle.truffle.api.TruffleFile;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.charset.Charset;
-import java.nio.file.StandardOpenOption;
 
 public class Detector implements TruffleFile.FileTypeDetector {
   @Override
-  public String findMimeType(TruffleFile file) throws IOException {
+  public String findMimeType(TruffleFile file) {
     String name = file.getName();
     if (name != null && name.endsWith(Language.EXTENSION)) return Language.MIME_TYPE;
-
-    //long magicWord = readMagicWord(file);
-    //if (magicWord == Language.BYTECODE_MAGIC_WORD) return Language.BYTECODE_MIME_TYPE;
     return null;
   }
 
   @Override
-  public Charset findEncoding(TruffleFile file) throws IOException {
+  public Charset findEncoding(TruffleFile file) {
     return null;
-  }
-
-  private static long readMagicWord(TruffleFile file) {
-    try (InputStream is = file.newInputStream(StandardOpenOption.READ)) {
-      byte[] buffer = new byte[4];
-      if (is.read(buffer) != buffer.length) return 0;
-      return Integer.toUnsignedLong(ByteBuffer.wrap(buffer).order(ByteOrder.nativeOrder()).getInt());
-    } catch (IOException | SecurityException e) {
-      return 0;
-    }
   }
 }
