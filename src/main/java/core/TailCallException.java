c069f689584d2f949a7cc49f64d8630fa1500cd4
Edward Kmett <ekmett@gmail.com>
2019-10-11 19:45:03 -0700

    fix version

--- src/main/java/core/TailCallException.java
+++ src/main/java/core/TailCallException.java
@@ -1,11 +1,10 @@
 package core;
 
-import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.nodes.ControlFlowException;
 import core.values.Closure;
 
 public class TailCallException extends ControlFlowException {
-    private static final long serialVersionUID = 4794670470976114363L;
+    private static final long serialVersionUID = 4749484936180230134L;
     public final Closure closure;
     public final Object[] arguments;
     public TailCallException(Closure closure, Object[] arguments) {
