19780b7930e609b05c5532e425c4d229597504d7
Edward Kmett <ekmett@gmail.com>
2019-10-26 03:36:30 -0700

    consolidate build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,6 +94,57 @@ distributions {
   }
 }
 
+project(":language") {
+  apply(plugin = "antlr")
+  apply(plugin = "kotlin")
+  apply(plugin = "kotlin-kapt")
+
+  val antlrRuntime by configurations.creating
+  val kotlinRuntime by configurations.creating
+
+  dependencies {
+    "kapt"("org.graalvm.truffle:truffle-api:19.2.0.1")
+    "kapt"("org.graalvm.truffle:truffle-dsl-processor:19.2.0.1")
+    "antlr"("org.antlr:antlr4:4.7.2")
+    antlrRuntime("org.antlr:antlr4-runtime:4.7.2")
+    compileOnly("org.graalvm.truffle:truffle-api:19.2.0.1")
+    compileOnly("org.graalvm.sdk:graal-sdk:19.2.0.1")
+    implementation("org.antlr:antlr4-runtime:4.7.2")
+    kotlinRuntime(kotlin("stdlib"))
+    kotlinRuntime(kotlin("stdlib-jdk8"))
+    testImplementation("org.testng:testng:6.14.3")
+  }
+
+  tasks.getByName<Jar>("jar") {
+    archiveBaseName.set("cadenza-language")
+    manifest {
+      attributes["Class-Path"] =  configurations.runtimeClasspath.get().files.map { it.getAbsolutePath() } .joinToString(separator = " ")
+    }
+  }
+
+  tasks.withType<AntlrTask> {
+    arguments.addAll(listOf("-package", "cadenza.syntax", "-no-listener", "-no-visitor"))
+  }
+}
+
+project(":launcher") {
+  dependencies {
+    implementation(project(":language"))
+    implementation("org.graalvm.truffle:truffle-api:19.2.0.1")
+    implementation("org.graalvm.sdk:launcher-common:19.2.0.1")
+  }
+  
+  tasks.getByName<Jar>("jar") {
+    archiveBaseName.set("cadenza-launcher")
+    manifest {
+      attributes["Main-Class"] = "cadenza.launcher.Launcher"
+      attributes["Class-Path"] = configurations.runtimeClasspath.get().files.map { it.getAbsolutePath() } .joinToString(separator = " ")
+    }
+  }
+}
+
+// calculate local platform values
+
 val jar = tasks.getByName<Jar>("jar") {
   baseName = "cadenza"
   description = "Build the cadenza component for graal"
@@ -147,7 +198,6 @@ tasks.withType<ProcessResources> {
   }
 }
 
-// calculate local platform values
 
 val os = System.getProperty("os.name")
 logger.info("os = {}",os)
@@ -268,3 +318,4 @@ tasks.withType<DokkaTask> {
     }
   }
 }
+
