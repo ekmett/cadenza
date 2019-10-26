3c11603e27d85a930512950a6af6beb329f1bdb5
Edward Kmett <ekmett@gmail.com>
2019-10-26 03:31:11 -0700

    remove needless attributes

--- language/build.gradle.kts
+++ language/build.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
   "kapt"("org.graalvm.truffle:truffle-dsl-processor:19.2.0.1")
   "antlr"("org.antlr:antlr4:4.7.2")
   antlrRuntime("org.antlr:antlr4-runtime:4.7.2")
-  implementation("org.graalvm.truffle:truffle-api:19.2.0.1")
-  implementation("org.graalvm.sdk:graal-sdk:19.2.0.1")
+  compileOnly("org.graalvm.truffle:truffle-api:19.2.0.1")
+  compileOnly("org.graalvm.sdk:graal-sdk:19.2.0.1")
   implementation("org.antlr:antlr4-runtime:4.7.2")
   kotlinRuntime(kotlin("stdlib"))
   kotlinRuntime(kotlin("stdlib-jdk8"))
@@ -19,19 +19,12 @@ dependencies {
 }
 
 tasks.getByName<Jar>("jar") {
-  baseName = "cadenza-language"
+  archiveBaseName.set("cadenza-language")
   manifest {
-    attributes["Main-Class"] = "cadenza.Main"
-    attributes["Specification-Title"] = "cadenza-language"
-    attributes["Specification-Version"] = project.version
-    attributes["Specification-Vendor"] = "Machine Intelligence Research Institute"
-    attributes["Implementation-Title"] = "cadenza-language"
-    attributes["Implementation-Version"] = project.version
-    attributes["Implementation-Vendor"] = "Machine Intelligence Research Institute"
     attributes["Class-Path"] =  configurations.runtimeClasspath.get().files.map { it.getAbsolutePath() } .joinToString(separator = " ")
   }
 }
 
 tasks.withType<AntlrTask> {
-  arguments.addAll(listOf("-package", "cadenza.syntax", "-no-listener", "-visitor"))
+  arguments.addAll(listOf("-package", "cadenza.syntax", "-no-listener", "-no-visitor"))
 }
--- launcher/build.gradle.kts
+++ launcher/build.gradle.kts
@@ -5,15 +5,9 @@ dependencies {
 }
 
 tasks.getByName<Jar>("jar") {
-  baseName = "cadenza-launcher"
+  archiveBaseName.set("cadenza-launcher")
   manifest {
     attributes["Main-Class"] = "cadenza.launcher.Launcher"
-    attributes["Specification-Title"] = "cadenza.launcher"
-    attributes["Specification-Version"] = project.version
-    attributes["Specification-Vendor"] = "Machine Intelligence Research Institute"
-    attributes["Implementation-Title"] = "cadenza.launcher"
-    attributes["Implementation-Version"] = project.version
-    attributes["Implementation-Vendor"] = "Machine Intelligence Research Institute"
     attributes["Class-Path"] = configurations.runtimeClasspath.get().files.map { it.getAbsolutePath() } .joinToString(separator = " ")
   }
 }
