29e10469adde131955849afcd6dd915ed128f3c3
Edward Kmett <ekmett@gmail.com>
2019-10-26 06:27:49 -0700

    fix panic

--- language/src/main/kotlin/cadenza/util/Errors.kt
+++ language/src/main/kotlin/cadenza/util/Errors.kt
@@ -3,10 +3,16 @@ package cadenza.util
 import com.oracle.truffle.api.CompilerDirectives
 
 object Errors {
-  fun panic(msg: String, base: Exception? = null): Nothing {
+  fun panic(msg: String, base: Exception?): Nothing {
     CompilerDirectives.transferToInterpreter();
-    val e = RuntimeException(msg, base);
-    e.stackTrace = e.stackTrace.dropLast(1).toTypedArray() // drop call to panic from trace
+    val e = RuntimeException(msg, base)
+    e.stackTrace = e.stackTrace.drop(1).toTypedArray()
+    throw e;
+  }
+  fun panic(msg: String): Nothing {
+    CompilerDirectives.transferToInterpreter();
+    val e = RuntimeException(msg, null)
+    e.stackTrace = e.stackTrace.drop(1).toTypedArray()
     throw e;
   }
 }
