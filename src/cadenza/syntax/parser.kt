ec156d4dfa12295aa9a8e0c8fcb6c86d915da52a
Edward Kmett <ekmett@gmail.com>
2019-12-01 15:50:38 -0800

    bikeshedding

--- src/cadenza/syntax/parser.kt
+++ src/cadenza/syntax/parser.kt
@@ -1,45 +1,38 @@
 package cadenza.syntax
 
-import cadenza.semantics.*
+import cadenza.semantics.Name
+import cadenza.semantics.Term
 import cadenza.semantics.Term.Companion.tlam
 import cadenza.semantics.Term.Companion.tvar
+import cadenza.semantics.Type
 import org.intelligence.parser.*
 
+val Parse.type: Type get() = choice({string("Nat"); Type.Nat })
+val Parse.ident: String get() = named("ident") {some { satisfy { it.isLetter() } }.joinToString() }
+val Parse.space: Unit get() { many { satisfy { it.isWhitespace() }} }
 
-// expr = char '(' <* expr *> char ')'
-//    <|> char '\' <* (Lam <$> many (var <* space) <*> expr)
-//    <|> Var <$> var
-
-
-val type: Parser<Type> = {choice({string("Nat"); Type.Nat })}
-
-val ident: Parser<String> = {named("ident") {some { satisfy { it.isLetter() } }.joinToString() } }
-
-val space: Parser<Unit> = {many { satisfy { it.isWhitespace() }}; Unit }
-
-fun <A> Parse.parens(f: Parse.() -> A): A {
-    char('(')
-    space()
-    val x = f()
-    char(')')
-    space()
-    return x
+inline fun <T,A> T.parens(f: T.() -> A): A where T : Parse {
+  char('(')
+  space
+  val x = f()
+  char(')')
+  space
+  return x
 }
 
-val tele: Parser<Array<Pair<Name,Type>>> = { some {
-    parens {val x = ident(); space(); char(':'); space(); val t = type(); Pair(x, t)}
-}.toTypedArray() };
+val Parse.tele: Array<Pair<Name,Type>> get() = some {
+  parens {val x = ident; space; char(':'); space; val t = type; Pair(x, t)}
+}.toTypedArray()
 
-fun Parse.grammar(): Term {
-    return choice({
-        char('\\')
-        val t = tele()
-        string("->")
-        space()
-        tlam(t, grammar())
-    }, {
-        tvar(ident())
-    }, {
-        parens { grammar() }
-    })
-}
+val Parse.grammar: Term get() =
+  choice({
+    char('\\')
+    val t = tele
+    string("->")
+    space
+    tlam(t, grammar)
+  }, {
+    tvar(ident)
+  }, {
+    parens { grammar }
+  })
