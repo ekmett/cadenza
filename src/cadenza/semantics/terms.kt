60e70cd115476eaa97eb6780d69233105510a056
Saura Charbonneau <zcarterc@gmail.com>
2019-12-03 15:08:00 -0800

    tlam, probably broken

--- src/cadenza/semantics/terms.kt
+++ src/cadenza/semantics/terms.kt
@@ -1,7 +1,9 @@
 package cadenza.semantics
 
-import cadenza.jit.Code
 import cadenza.*
+import cadenza.jit.*
+import com.oracle.truffle.api.RootCallTarget
+import com.oracle.truffle.api.Truffle
 import com.oracle.truffle.api.frame.FrameDescriptor
 
 typealias Ctx = Env<Type>
@@ -72,7 +74,59 @@ abstract class Term {
     @Suppress("UNUSED_PARAMETER","unused")
     fun tlam(names: Array<Pair<Name,Type>>, body: Term, loc: Loc? = null): Term = object : Term() {
       override fun infer(ctx: Ctx): Witness {
-        todo
+        var ctx2 = ctx;
+        for ((n,ty) in names) {
+          ctx2 = ConsEnv(n, ty, ctx)
+        }
+        val bodyw = body.infer(ctx2)
+        var aty = bodyw.type
+        for ((_,ty) in names.reversed()) {
+          aty = Type.Arr(ty, aty)
+        }
+        val arity = names.size
+        return object : Witness(aty) {
+          // looks at what vars the body adds to it's FrameDescriptor to decide what to capture
+          // TODO: maybe should calculate fvs instead?
+          override fun compile(fd: FrameDescriptor): Code {
+            val bodyFd = FrameDescriptor()
+            val bodyCode = bodyw.compile(bodyFd)
+            // used as spec for materialized frame in closure
+            val closureFd = FrameDescriptor()
+            var closureCaptures: Array<FrameBuilder> = arrayOf();
+            var envPreamble: Array<FrameBuilder> = arrayOf();
+            var argPreamble: Array<FrameBuilder> = arrayOf();
+
+            val captures = bodyFd.identifiers.any { n -> names.find { it.first == n } == null }
+
+            for (name in bodyFd.identifiers) {
+              val bodySlot = bodyFd.findFrameSlot(name)
+
+              val ix = names.indexOfLast {it.first == name}
+              if (ix != -1) {
+                argPreamble += put(bodySlot,Code.Arg(if (captures) (ix + 1) else ix))
+              } else {
+                val closureSlot = closureFd.addFrameSlot(name)
+                val parentSlot = fd.findOrAddFrameSlot(name)
+
+                closureCaptures += put(closureSlot,Code.`var`(parentSlot))
+                envPreamble += put(bodySlot,Code.`var`(closureSlot))
+              }
+            }
+
+            assert((!captures) || envPreamble.isNotEmpty())
+
+            val bodyBody = ClosureBody(bodyCode)
+
+            // TODO: does this need to use a builder?
+            // is this the right way to get the TruffleLanguage?
+            val rootNode = ClosureRootNode(Language(), bodyFd, arity, envPreamble, argPreamble, bodyBody)
+
+            // TODO: is this right?
+            val callTarget: RootCallTarget = Truffle.getRuntime().createCallTarget(rootNode)
+
+            return Code.lam(closureFd, closureCaptures, callTarget, aty)
+          }
+        }
       }
     }
   }
